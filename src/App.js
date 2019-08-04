e2d8000cfc05ea891497a0f9bd13589f2675fa57
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-08-04 20:57:05 +0530

    add componentDidCatch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import StudentForm from './components/StudentForm';
+import StudentForm from "./components/StudentForm";
 
 class App extends Component {
-  
+  state = {
+    hasError: false
+  };
+  componentDidCatch() {
+    this.setState({
+      hasError: true
+    });
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+    if (this.state.hasError) {
+      return <h2>Oh no! Something's wrong!</h2>;
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
 
-        <StudentForm />
-      </div>
-    );
+          <StudentForm />
+        </div>
+      );
+    }
   }
 }
 
