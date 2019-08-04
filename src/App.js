f6f2a1dfd9e81d511551c0f1ee81535f64e51ac2
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-08-04 23:13:11 +0530

    add modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary";
-
 import StudentForm from "./components/StudentForm";
+import Modal from "./components/Modal";
 
 class App extends Component {
   render() {
@@ -16,6 +17,11 @@ class App extends Component {
         <ErrorBoundary>
           <StudentForm />
         </ErrorBoundary>
+        <Modal>
+          <div className="modal">
+            <h1>Hi from modal window</h1>
+          </div>
+        </Modal>
       </div>
     );
   }
